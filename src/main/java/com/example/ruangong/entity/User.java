c66e2216030535fe8211bd5670e35e9fe626f49a
Ychomma <50377560@qq.com>
2022-11-15 16:27:16 +0800

    1

--- src/main/java/com/example/ruangong/entity/User.java
+++ src/main/java/com/example/ruangong/entity/User.java
@@ -9,5 +9,4 @@ import java.util.List;
 public class User {
     private int id; // 唯一表示符用户id
     private ArrayList<Integer> achievementsDone; // 用户已触发的成就列表
-    private int cnt; // 用户玩的轮次计数,问号表示可有可无
 }

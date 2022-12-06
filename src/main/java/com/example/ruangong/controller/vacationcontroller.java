731f1fb47c85120a4d8ccc1c94c555dde83d6922
Ychomma <50377560@qq.com>
2022-12-06 20:54:55 +0800

    update

--- src/main/java/com/example/ruangong/controller/vacationcontroller.java
+++ src/main/java/com/example/ruangong/controller/vacationcontroller.java
@@ -1,5 +1,6 @@
 package com.example.ruangong.controller;
 
+import com.example.ruangong.entity.Returnvacation;
 import com.example.ruangong.entity.Vacation;
 import com.example.ruangong.jwt.JwtUtils;
 import com.example.ruangong.mapper.vacationmapper;
@@ -17,8 +18,32 @@ import java.util.Random;
 @RestController
 @RequestMapping
 public class vacationcontroller {
-
-
-
+      @Autowired
+      private vacationmapper vac;
+      @GetMapping("/choices")
+      public List<Returnvacation> vacation_event(){
+        List<Vacation> ac = vac.find();
+        List<Returnvacation> a = new ArrayList<>();
+        int flag[] = new int[ac.size()];
+        for (int i = 0; i < ac.size(); i++) {
+            flag[i] = 0;
+        }
+        for (; a.size() < 7; ) {
+            Returnvacation b = new Returnvacation();
+            Random r = new Random();
+            int number = r.nextInt(ac.size());
+            if (flag[number] == 0) {
+                b.setContent(ac.get(number).getContent());
+                String[] temp1 = ac.get(number).getProperty_change().split(" ");
+                int[] temp2 = new int[temp1.length];
+                for(int j=0;j<temp1.length;j++)
+                    temp2[j]=Integer.parseInt(temp1[j]);
+                b.setPropertyChange(temp2);
+                a.add(b);
+                flag[number] = 1;
+            } else continue;
+        }
+            return a;
+    }
     }
 
---
+++ src/main/java/com/example/ruangong/entity/Returnvacation.java
@@ -0,0 +1,9 @@
+package com.example.ruangong.entity;
+
+import lombok.Data;
+
+@Data
+public class Returnvacation {
+    private int[] propertyChange;
+    private String content;
+}

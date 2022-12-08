fe1b60b55991663d03f342059884dbc331a80e18
Ychomma <50377560@qq.com>
2022-12-08 16:25:38 +0800

    update

--- src/main/java/com/example/ruangong/entity/Achievement.java
+++ src/main/java/com/example/ruangong/entity/Achievement.java
@@ -10,6 +10,7 @@ public class Achievement {
     private String event_need;// 触发需要的事件id列表，可以为空
     private String property_need;// 触发需要的属性列表,长6，可以为空
     private int id; // 唯一标识id
-    private String rarity; // 颜色（稀有度）
+    private int rarity; // 颜色（稀有度）
     private String name;
+    private String type;
 }

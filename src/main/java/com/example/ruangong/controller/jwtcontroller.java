c7e2f1de2bb07e0fde65af3f6890a9f1222bd23d
Ychomma <50377560@qq.com>
2022-12-08 21:10:05 +0800

    update

--- src/main/java/com/example/ruangong/controller/jwtcontroller.java
+++ src/main/java/com/example/ruangong/controller/jwtcontroller.java
@@ -1,11 +1,14 @@
 package com.example.ruangong.controller;
 
 import com.example.ruangong.entity.Returntoken;
+import com.example.ruangong.entity.User;
 import com.example.ruangong.jwt.JwtUtils;
 import com.example.ruangong.mapper.usermapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,21 +17,22 @@ import java.util.Date;
 @RequestMapping
 public class jwtcontroller {
     @Autowired
-    JwtUtils jwt;
-    @Autowired
     usermapper u;
     Returntoken re = new Returntoken();
-    @GetMapping("/")
-    public Returntoken token(String s){
+    @GetMapping("/token")
+    public String token(){
         Date t = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String p = simpleDateFormat.format(t);
         //u.insert(p);
-        try{
-            re.setUser(u.find(jwt.getDate(s)));
-        }catch (Exception e){
-            re.setToken(jwt.createToken(p));
-        }
-        return re;
+        return JwtUtils.createToken(p);
+    }
+    @GetMapping("/user/data")
+    protected User doget(@RequestHeader("lifestartToken") String req){
+            try{
+                return u.find(JwtUtils.getDate(req));
+            }catch (Exception e){
+                return null;
+            }
     }
 }
--- src/main/java/com/example/ruangong/entity/User.java
+++ src/main/java/com/example/ruangong/entity/User.java
@@ -9,7 +9,8 @@ import java.util.List;
 @Data
 public class User {
     private int id; // 唯一表示符用户id
-    private String achievement_done; // 用户已触发的成就列表
-    private int cnt; //触发成就数目
-    private String createtime;
+    private int[] commonAchievementList; // 用户已触发的成就列表
+    private int[] specialAchievementList; //触发成就数目
+    private String createTime;
+    private String restartNum;
 }

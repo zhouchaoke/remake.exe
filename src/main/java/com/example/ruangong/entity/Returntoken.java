7358f9e46d1ef3d2749b59445a5cea1e6efb5c76
Ychomma <50377560@qq.com>
2022-12-16 21:38:28 +0800

    update

--- src/main/java/com/example/ruangong/entity/Returntoken.java
+++ src/main/java/com/example/ruangong/entity/Returntoken.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Returntoken {
-    private int[] commonAchievemntList;
+    private int[] commonAchievementList;
     private int[] specialAchievementList;
     private int restartNum;
 }
